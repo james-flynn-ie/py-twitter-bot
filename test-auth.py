4ebaf050cfc748b609cf6689c1ec3a699db52c72
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-12-25 19:23:25 +0000

    Checking that mandatory env vars are set in test-auth

--- test-auth.py
+++ test-auth.py
@@ -1,6 +1,12 @@
 import tweepy
 import os
 
+MANDATORY_ENV_VARS = ["TWITTER_CONSUMER_API_KEY", "TWITTER_CONSUMER_API_SECRET", "TWITTER_ACCESS_TOKEN", "TWITTER_ACCESS_TOKEN_SECRET"]
+
+for env_var in MANDATORY_ENV_VARS:
+  if env_var not in os.environ:
+    raise EnvironmentError("{} not found. Twitter App keys must be set as environment variables.".format(env_var))
+
 auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_API_KEY'], 
     os.environ['TWITTER_CONSUMER_API_SECRET'])
 auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], 
