1fafa2b39140b6f849948940d03b67ba91e59c62
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-12-25 19:43:42 +0000

    Refactor: Create main() in test-auth

--- test-auth.py
+++ test-auth.py
@@ -1,24 +1,28 @@
 import tweepy
 import os
 
-def verify_env_vars():
+def check_env_vars_exist():
   MANDATORY_ENV_VARS = ["TWITTER_CONSUMER_API_KEY", "TWITTER_CONSUMER_API_SECRET", "TWITTER_ACCESS_TOKEN", "TWITTER_ACCESS_TOKEN_SECRET"]
   
   for env_var in MANDATORY_ENV_VARS:
     if env_var not in os.environ:
       raise EnvironmentError("{} not found. Twitter App keys must be set as environment variables.".format(env_var))
 
-verify_env_vars()
+def main():
+  check_env_vars_exist()
+  
+  auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_API_KEY'], 
+      os.environ['TWITTER_CONSUMER_API_SECRET'])
+  auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], 
+      os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+  
+  api = tweepy.API(auth)
+  
+  try:
+      api.verify_credentials()
+      print("Authentication OK")
+  except:
+      print("Error during authentication")
 
-auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_API_KEY'], 
-    os.environ['TWITTER_CONSUMER_API_SECRET'])
-auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], 
-    os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
-
-api = tweepy.API(auth)
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+if __name__== "__main__":
+  main()
