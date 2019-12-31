251fcdf58fc23d6e91cf99cc9c3e71006a747c40
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-12-31 00:02:41 +0000

    Adding auth Tweepy API instantiation

---
+++ create-api.py
@@ -0,0 +1,32 @@
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+def set_auth(consumer_key, consumer_secret, access_token, access_token_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    return auth
+
+def create_api():
+    consumer_key = os.getenv("TWITTER_CONSUMER_API_KEY")
+    consumer_secret = os.getenv("TWITTER_CONSUMER_API_SECRET")
+    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+    access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+    auth = set_auth(consumer_key, consumer_secret, access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+
+if __name__ == '__main__':
+    create_api()
