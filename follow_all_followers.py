92850924de21110982bdd8d49ec375fe2b30363f
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-12-31 17:02:20 +0000

    Added try-except block around API calls

--- follow_all_followers.py
+++ follow_all_followers.py
@@ -8,24 +8,28 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 def follow_followers(api):
-    logger.info("Retrieving list of followers")
+    logger.info("Retrieving list of followers.")
 
-    for follower in tweepy.Cursor(api.followers).items(0):
-        logger.debug(f"Checking if {follower.name} is being followed yet")
-        if not follower.following:
-            logger.info(f"Following {follower.name}")
-            follower.follow()
-    
-    logger.info("Follower list check complete!")
+    try:
+        for follower in tweepy.Cursor(api.followers).items():
+            logger.debug(f"Checking if {follower.name} is being followed yet")
+            if not follower.following:
+                logger.info(f"Following {follower.name}")
+                follower.follow()
+    except Exception as e:
+        logger.error(f"Failed to follow {follower.name}", exc_info=True)
+        raise e
+
+    logger.info("Finished checking follower list!")
 
 def main():
-    api = create_api.main()
-
     seconds_between_checks = 900
 
+    api = create_api.main()
+    
     while True:
         follow_followers(api)
-        logger.debug(f"Waiting for {seconds_between_checks} seconds...")
+        logger.debug(f"Waiting for {seconds_between_checks} seconds.")
         time.sleep(seconds_between_checks)
 
 if __name__ == "__main__":
