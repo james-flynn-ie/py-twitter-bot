e8056a3ce52aca2f07c109bfeb2a764b4fa7d419
James Flynn <james-flynn-ie@users.noreply.github.com>
2021-06-07 09:03:06 +0100

    Changed: Fixed linting

--- test_auth.py
+++ test_auth.py
@@ -2,32 +2,39 @@ from create_api import set_auth
 import os
 import tweepy
 
+
 def check_env_vars_exist():
-  MANDATORY_ENV_VARS = ["TWITTER_CONSUMER_API_KEY",
-                        "TWITTER_CONSUMER_API_SECRET",
-                        "TWITTER_ACCESS_TOKEN",
-                        "TWITTER_ACCESS_TOKEN_SECRET"]
-  
-  for env_var in MANDATORY_ENV_VARS:
-    if env_var not in os.environ:
-      raise EnvironmentError("{} not found. Twitter App keys must be set as environment variables.".format(env_var))
+    MANDATORY_ENV_VARS = ["TWITTER_CONSUMER_API_KEY",
+                          "TWITTER_CONSUMER_API_SECRET",
+                          "TWITTER_ACCESS_TOKEN",
+                          "TWITTER_ACCESS_TOKEN_SECRET"]
+
+    for env_var in MANDATORY_ENV_VARS:
+        if env_var not in os.environ:
+            raise EnvironmentError(
+                "{} not found. Set Twitter App keys as environment variables."
+                .format(env_var)
+                )
+
 
 def main():
-  check_env_vars_exist()
+    check_env_vars_exist()
 
-  auth = set_auth(os.environ['TWITTER_CONSUMER_API_KEY'],
-                  os.environ['TWITTER_CONSUMER_API_SECRET'],
-                  os.environ['TWITTER_ACCESS_TOKEN'],
-                  os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+    auth = set_auth(os.environ['TWITTER_CONSUMER_API_KEY'],
+                    os.environ['TWITTER_CONSUMER_API_SECRET'],
+                    os.environ['TWITTER_ACCESS_TOKEN'],
+                    os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
 
-  api = tweepy.API(auth)
-  
-  try:
-      api.verify_credentials()
-      print("Authentication OK")
-  except:
-      print("Authentication failed." +
-            "\nCheck that you have set the correct Consumer API and Access Token key values.")
+    api = tweepy.API(auth)
+
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Authentication failed." +
+              "\nCheck that you have set the correct Consumer API " +
+              "and Access Token key values.")
+
 
 if __name__ == "__main__":
-  main()
+    main()
